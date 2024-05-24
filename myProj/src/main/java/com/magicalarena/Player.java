653b6591b8f9459fbb0fdaafa2e265f48b08b746
Nitin-Rajasekar <nitin.rajasekar@students.iiit.ac.in>
2024-05-24 19:33:15 +0530

    Adding name and colour_code attribute to player class.

--- myProj/src/main/java/com/magicalarena/Player.java
+++ myProj/src/main/java/com/magicalarena/Player.java
@@ -4,11 +4,15 @@ public class Player {
     private int health;
     private int strength;
     private int attack;
+    private String name;
+    private String colour_code;
 
-    public Player(int health, int strength, int attack) {
+    public Player(int health, int strength, int attack, String name, String colour_code) {
         this.health = health;
         this.strength = strength;
         this.attack = attack;
+        this.name = name;
+        this.colour_code = colour_code;
     }
 
     public int getHealth() {
@@ -25,6 +29,14 @@ public class Player {
 
     public int getAttack() {
         return attack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColour_code() {
+        return colour_code;
     }
 
     public boolean isAlive() {
